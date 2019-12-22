88555db432772817dddef284931cc4e8c21962f8
vyava <zafergenc02@gmail.com>
2019-12-22 17:57:48 +0300

    temp _Menu component removed

--- src/client/components/_Menu/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style';
-const Menu: React.FunctionComponent = ({ children }) => {
-  return <nav className="Navbar_Container">{children}</nav>;
-};
-
-export default Menu;
