2e34f205239b2ec8e1422a9356154f872a97d8f4
unknown <zafergenc02@gmail.com>
2019-12-23 11:10:41 +0300

    [NEW] FontAwesome added

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -6,6 +6,14 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './router';
 
+// Font Awesome Imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// Library Creation
+library.add(fab, fas);
+
 const { lazy, Suspense } = React;
 
 const App = lazy(() => import('./containers/App/index'));
