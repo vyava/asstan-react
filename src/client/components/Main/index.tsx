6ff2e5f78159dd2611bc82461f44ec8e2f745472
unknown <zafergenc02@gmail.com>
2019-12-27 14:11:26 +0300

    fake loading state

--- src/client/components/Main/index.tsx
+++ src/client/components/Main/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles';
-import { Collapse } from "antd";
+import classNames from "classnames";
+import { Collapse, Skeleton } from "antd";
 import List from '@/components/List';
 import Map from '@/components/Map';
 
 import { bayiList } from '../../../../__mocks__/db';
 
-const {Panel} = Collapse
+const { Panel } = Collapse
 const Main = () => {
 
   // const onChange = (params : any, another : any) => {
   //   console.log(params)
   // }
+
+  let [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 3000)
+
   return (
     <>
       <div className="corner"></div>
@@ -21,16 +29,18 @@ const Main = () => {
             <List.ListFilter />
             <List.SearchResult />
           </div>
-          <div className="Item_Container">
+          <div className={classNames({"Item_Container" : true, loading : loading})}>
+            <Skeleton loading={loading} active={loading} />
             <Collapse destroyInactivePanel accordion>
               {
                 bayiList.map(bayi => (
-                  <Panel key={bayi._id} header={<List.Item  bayi={bayi} />}>
+                  <Panel key={bayi._id} header={<List.Item bayi={bayi} />}>
                     <p>{bayi.adres}</p>
                   </Panel>
                 ))
               }
             </Collapse>
+
           </div>
         </List>
         <div className="Main_Content_Controller">
