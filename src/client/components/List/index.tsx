04189e16ea231c76ef7c6ba817657d543d6897cc
vyava <zafergenc02@gmail.com>
2020-01-02 15:06:50 +0300

    Test api call removed

--- src/client/components/List/index.tsx
+++ src/client/components/List/index.tsx
@@ -1,6 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
-
-import fetch from "isomorphic-fetch";
+import React, { ReactNode } from 'react';
 
 import './style';
 import ListFilter from '@/components/List/ListFilter';
@@ -13,21 +11,8 @@ interface IListProps {
 
 const List = ({children} : IListProps) => {
 
-  let [bayiList, setBayiList] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `http:localhost:7000/api/customers`,
-      {
-        method : 'GET',
-      }
-    )
-      .then((data : any) => {setBayiList(data)})
-  }, [])
-
   return (
     <div className="List_Container">
-      {JSON.stringify(bayiList)}
       {children}
     </div>
   );
